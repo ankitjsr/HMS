b0a0cd2220a3198e632d1fa401a8c595b01e774f
Ankit Kumar <ankitkumar5327@gmail.com>
2019-06-04 00:55:06 +0530

    initialized

---
+++ HMS.Entities/AccomodationPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class AccomodationPackage
+    {
+        public int Id { get; set; }
+
+        public int AccmodationTypeId { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+
+        public string Name { get; set; }
+        public int NoOfRooms { get; set; }
+        public decimal FeePerNight { get; set; }
+    }
+}
---
+++ HMS.Entities/Bookings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class Bookings
+    {
+        public int Id { get; set; }
+
+        public Accomodation AccomodationId  { get; set; }
+        public Accomodation Accomodation { get; set; }
+
+        public DateTime FromDate { get; set; }
+        /// <summary>
+        /// Number of nights
+        /// </summary>
+        public int Duration { get; set; }
+
+
+    }
+}
